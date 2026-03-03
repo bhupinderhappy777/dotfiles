c17efa404f2d36fe19ba8e1e5b5782481a3e32fa
Bhupinder Singh Gill <bhupinderhappy777@gmail.com>
2026-03-03 09:32:32 -0800

    cleaned v6 dot files for plasma

--- plasma/v6/apply_layout.sh
+++ plasma/v6/apply_layout.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-export DISPLAY=:0
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
-# 1. Get the REAL Activity ID of your current session
-# This ensures the desktop "Containment" actually connects to your screen
-CURRENT_ACT=$(dbus-send --session --print-reply --dest=org.kde.ActivityManager /ActivityManager/Activities org.kde.ActivityManager.Activities.CurrentActivity | grep string | cut -d '"' -f 2)
+# 1. Detect Activity (Universal method)
+CURRENT_ACT=$(dbus-send --session --print-reply --dest=org.kde.ActivityManager /ActivityManager/Activities org.kde.ActivityManager.Activities.CurrentActivity | grep string | cut -d '"' -f 2 | tr -d '[:space:]')
 
-# 2. Stop the shell safely
-systemctl --user stop plasma-plasmashell.service
-killall -9 plasmashell 2>/dev/null
-sleep 3
+# 2. Kill services properly
+systemctl --user stop plasma-plasmashell.service xdg-desktop-portal.service
+killall -9 plasmashell xdg-desktop-portal-kde 2>/dev/null
+sleep 2
 
-# 3. Copy the master file and IMMEDIATELY patch the ID
-cp "$HOME/dotfiles/plasma/v6/appletsrc.master" "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc"
-sed -i "s/activityId=[a-z0-9-]*/activityId=$CURRENT_ACT/g" "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc"
+# 3. Apply patched config
+sed "s/activityId=[a-fA-F0-9\-]*/activityId=$CURRENT_ACT/g" "$HOME/dotfiles/plasma/v6/appletsrc.master" > "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc"
 
-# 4. Wipe caches and restart
+# 4. Deep cache wipe (Prevents the SIGABRT)
 rm -rf ~/.cache/plasmashell
+rm -rf ~/.cache/plasma*
+rm -f ~/.cache/*.kcache
+kbuildsycoca6 --noincremental
+
+# 5. Start
+systemctl --user start xdg-desktop-portal.service
+sleep 2
 systemctl --user start plasma-plasmashell.service
